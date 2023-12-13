30818c9275332ff1faf87e858051f8c34a210b3a
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-13 12:52:31 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double a,b,c;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("a = ");
+        a=sc.nextDouble();
+        System.out.print("b = ");
+        b=sc.nextDouble();
+        System.out.print("c = ");
+        c=sc.nextDouble();
+        if(a<c && b<c){
+            if(b<c){
+                System.out.println(a + " "+ b+" "+c);
+            }else {
+                System.out.println(a + " "+ c+" "+b);
+            }
+        } else if (b<c && b<a){
+            if(a<c){
+                System.out.println(b + " "+ a+" "+c);
+            }else {
+                System.out.println(b+ " "+ c+" "+b);
+            }
+
+            }else{if(a<b){
+            System.out.println(c + " "+ a+" "+b);
+        }else {
+            System.out.println(c+ " "+ b+" "+a);
+        }
     }
-}
+}}
